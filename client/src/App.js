fefd824d6793f82c365d7e916250a1fe2e5a500f
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-03-04 08:54:06 +0100

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,28 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import {
-  faCalendarAlt, faClock, faMapPin, faAngleDown, faAngleLeft, faAngleRight, faCheckCircle, faTrashAlt, faEdit, faUserPlus, faUserMinus, faLink, faTimesCircle, faUsers, faBan, faSortAlphaDown
+  faCalendarAlt, faClock, faMapPin, faAngleDown, faAngleLeft, faAngleRight, faCheckCircle, faTrashAlt, faEdit, faUserPlus, faUserMinus, faLink, faTimesCircle, faUsers, faBan, faSortAlphaDown, faDesktop, faEnvelope
 } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./components/Main";
 import ContextRegistration from "./contexts/eventRegistration/Context";
+import Footer from './components/Footer';
 
-library.add(faCalendarAlt, faClock, faMapPin, faAngleDown, faAngleLeft, faAngleRight, faCheckCircle, faTrashAlt, faEdit, faUserPlus, faUserMinus, faLink, faTimesCircle, faUsers, faBan, faSortAlphaDown);
+library.add(fab, faCalendarAlt, faClock, faMapPin, faAngleDown, faAngleLeft, faAngleRight, faCheckCircle, faTrashAlt, faEdit, faUserPlus, faUserMinus, faLink, faTimesCircle, faUsers, faBan, faSortAlphaDown, faDesktop, faEnvelope);
 
 function App() {
   return (
     <Router>
       <ContextRegistration>
-      <div className="App">
-        <Navbar />
-        <Body>
-          <Main />
-        </Body>
-      </div>
+        <div className="App">
+          <Navbar />
+          <Body>
+            <Main />
+          </Body>
+          <Footer />
+        </div>
       </ContextRegistration>
     </Router>
   );
---
+++ client/src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Footer = props => {
+  return (
+    <div id="footer">
+      <div id="footer-content">
+        <a href="https://www.facebook.com/KummissjoniEwropea/" target="blank">
+          <h5>
+            <FontAwesomeIcon icon={["fab", "facebook"]} size="2x" />
+          </h5>
+        </a>
+        <a href="https://twitter.com/ECRepMalta" target="blank">
+          <h5>
+            <FontAwesomeIcon icon={["fab", "twitter"]} size="2x" />
+          </h5>
+        </a>
+        <a href="https://www.instagram.com/euinmalta/" target="blank">
+          <h5>
+            <FontAwesomeIcon icon={["fab", "instagram"]} size="2x" />
+          </h5>
+        </a>
+        <a href="https://ec.europa.eu/malta/" target="blank">
+          <h5>
+            <FontAwesomeIcon icon="desktop" size="2x" />
+          </h5>
+        </a>
+        <a href="mailto:comm-rep-mt@ec.europa.eu" target="blank">
+          <h5>
+            <FontAwesomeIcon icon="envelope" size="2x" />
+          </h5>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;

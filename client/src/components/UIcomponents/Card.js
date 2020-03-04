1f7a4ef08567336d75316ace129dc0a73c130316
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-03-04 13:00:43 +0100

    refactored header in registration

--- client/src/components/UIcomponents/Card.js
+++ client/src/components/UIcomponents/Card.js
@@ -1,9 +1,11 @@
 import React from "react";
+import RegistrationContext from "../../contexts/eventRegistration/RegistrationContext";
 
 const Card = props => {
-  
+  const context = React.useContext(RegistrationContext);
+  const {isRegistering} = context;
   return (
-    <div id="card">
+    <div id={isRegistering ? "card-to-collapse" : "card"}>
       {props.children}
     </div>
   );

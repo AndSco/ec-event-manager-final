9b5ffda3ee3cb797a7b4a7328d5485978357c20e
Andrea Scorcia <andrea.scorcia@gmail.com>
2020-03-04 07:53:41 +0100

    fix section title on mobile

--- client/src/components/UIcomponents/SectionTitle.js
+++ client/src/components/UIcomponents/SectionTitle.js
@@ -3,7 +3,7 @@ import React from "react";
 const SectionTitle = props => {
   return (
     <h3 className="section-title">
-      <i>{props.title}</i> <span>{props.callToAction}</span>
+      <i id="main-title">{props.title}</i> <span>{props.callToAction}</span>
     </h3>
   );
 }
